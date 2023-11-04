96ddd776507be9f88d08eb1e70c91b97448a7bf1
arumi123 <kimusyunapple@gmail.com>
2023-11-04 18:55:45 +0900

    When finished until 4_3

---
+++ 4_1_2/4_1_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+
+	printf("%d+%d=%d\n",100,200,100+200);
+	return 0;
+
+}
---
+++ 4_3/4_3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void){
+
+	printf("%f\n",10.0/3.0);
+	return 0;
+
+}
+
+
+
