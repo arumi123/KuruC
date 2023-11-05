4db4dc42ed35858cd0ee94bbaed8c79ec97472a2
arumi123 <kimusyunapple@gmail.com>
2023-11-05 22:02:48 +0900

    6_1

---
+++ 6_1/6_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+
+	int data;
+	double data2;
+
+	scanf("%d",&data);
+	printf("%d\n",data);
+
+	scanf("%lf",&data2);
+        printf("%f\n",data2);
+
+	return 0;
+
+}
